bdcb16957ab5daef4d4a82b85cc93b7cc29ee346
Monis9451 <mianmonishussain786@gmail.com>
2024-06-06 13:08:37 +0500

    These are the code and database files

---
+++ C# file.cs
@@ -0,0 +1,7 @@
+// All code is in a compress file with database
+// Database have 9 tables in total and it is linked with my database
+// To run the project in your systems follow the steps:
+// 1.Run the "DBS Project" file in your VS or VS code.
+// 2.Make a new Database query in your SQL server.
+// 3.Copy the address of your database and paste it in every form in VS/VScode where the linked
+// 4.If still you're stuck, Try a youtube video "How to connect c# file with database"
